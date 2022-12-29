953d41053fc8579a64c627354ca3bfcdea811e14
Sheung Wan Wong <melodywonghxy@hotmail.com>
2022-12-29 13:53:56 -0700

    feat: Add a event listener to each table cell to change background color of it to the selected color after a click

--- designs.js
+++ designs.js
@@ -1,10 +1,12 @@
-// Select color input
-const color = document.getElementById('colorPicker').value;
-
-// Select size input
-
-
-
+//by clicking submit button, first remove all the table rows
+function resetGrid() {
+    const table = document.getElementById('pixelCanvas');
+    let firstRow = table.firstElementChild;
+    while (firstRow) {
+        firstRow.remove();
+        firstRow = table.firstElementChild;
+    }
+ }
 
 //by clicking submit button, after resetGrid(), create a table with inputHeight and inputWidth using nested for loop
 function makeGrid() {
@@ -15,24 +17,19 @@ function makeGrid() {
     for (let row = 0; row < height; row++) {
         const newTableRow = document.createElement('tr');
         table.appendChild(newTableRow);
-
         //create width number of td and add the newTableCell to the newTableRow
         for (let col = 0; col < width; col++) {
             const newTableCell = document.createElement('td');
+            /*add a event listener to each grid square so that when a grid square is clicked, the background color of it
+            then gets changed to the selected color*/
+            newTableCell.addEventListener('click', function (event) {
+                const color = document.getElementById('colorPicker').value;
+                event.target.style.backgroundColor = color;
+            });
             newTableRow.appendChild(newTableCell);
         }
     }
 }
-
-//by clicking submit button, remove all the table rows
-function resetGrid() {
-    const table = document.getElementById('pixelCanvas');
-    let firstRow = table.firstElementChild;
-    while (firstRow) {
-        firstRow.remove();
-        firstRow = table.firstElementChild;
-    }
- }
 
 // When inputHeight and inputWidth are submitted by the user, call resetGrid() then makeGrid()
 document.addEventListener('submit', function (event) {

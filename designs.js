a3e44fa325a1eae3f025b1a17465f5ea4780be4d
Sheung Wan Wong <melodywonghxy@hotmail.com>
2022-12-28 23:10:07 -0700

    feat: Create makeGrid()

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
 // Select color input
+const color = document.getElementById('colorPicker').value;
+
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
 
+
+
+//create a table  with inputHeight and inputWidth using nested for loop
 function makeGrid() {
+    const table = document.getElementById('pixelCanvas');
+    const height = document.getElementById('inputHeight').value;
+    const width = document.getElementById('inputWidth').value;
+    //create height number of tr and add the newTableRow to the table
+    for (let row = 0; row < height; row++) {
+        const newTableRow = document.createElement('tr');
+        table.appendChild(newTableRow);
 
-// Your code goes here!
-
+        //create width number of td and add the newTableCell to the newTableRow
+        for (let col = 0; col < width; col++) {
+            const newTableCell = document.createElement('td');
+            newTableRow.appendChild(newTableCell);
+        }
+    }
 }
+// When size is submitted by the user, call makeGrid()
+document.addEventListener('submit', function (event) {
+    event.preventDefault();
+    makeGrid();
+});
+// TODO:
